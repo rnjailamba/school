0714d2337c07a1182e7d37cb5313d4fe909042b0
rnjai <rnjailamba12@gmail.com>
2016-05-30 19:38:52 +0530

    blog listing page is perfect with correct ordering and image size and pagination

--- routes/views/blog1.js
+++ routes/views/blog1.js
@@ -16,33 +16,6 @@ exports = module.exports = function (req, res) {
 	locals.posts = [];
 	locals.categories = [];
 
-	// Load all categories
-	view.on('init', function (next) {
-
-		PostCategory.model.find().sort('name').exec(function (err, results) {
-
-			if (err || !results.length) {
-				return next(err);
-			}
-
-			locals.categories = results;
-
-			// Load the counts for each category
-			async.each(locals.categories, function (category, next) {
-
-				keystone.list('Post').model.count().where('state', 'published').where('categories').in([category.id]).exec(function (err, count) {
-					category.postCount = count;
-					next(err);
-				});
-
-			}, function (err) {
-				next(err);
-			});
-
-		});
-
-	});
-
 	// Load the current category filter
 	view.on('init', function (next) {
 		if (req.params.category) {
@@ -60,11 +33,11 @@ exports = module.exports = function (req, res) {
 
 		var q = Post.paginate({
 				page: req.query.page || 1,
- 				perPage: 10,
- 				maxPages: 10,
+ 				perPage: 5,
+ 				maxPages: 1000,
 			})
 			.where('state', 'published')
-			.sort('-publishedDate')
+			.sort('publishedDate')
 			.populate('author categories');
 
 		if (locals.category) {
