0da041f38b66aa4f3da7d3ea80edfff57d818fd7
rnjai <rnjailamba12@gmail.com>
2016-06-20 05:59:16 +0530

    added pdf to notifs

--- models/Notification.js
+++ models/Notification.js
@@ -7,8 +7,10 @@ var Notification = new keystone.List('Notification', {
 
 Notification.add({
 	name: { type: String, required: true },
-	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
+	state: { type: Types.Select, options: 'draft, published, archived', default: 'published', index: true },
 	author: { type: Types.Relationship, ref: 'User', index: true },
+	pdfName: { type: String, index: true,default: 'Download File' },
+	pdfURL: { type: String},
 	publishedDate: { type: Types.Date, index: true },
 	content: { type: Types.Html, wysiwyg: true, height: 150  }
 });
